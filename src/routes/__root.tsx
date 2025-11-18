9eb9b86e44adde2ecdc6eef0fa341f3d97b48d97
ShoopDeWoop <116699852+Rikuron@users.noreply.github.com>
2025-11-18 12:42:27 +0800

    Removed TanStackDevTools from Production

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,23 +1,12 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
-import { TanStackDevtools } from '@tanstack/react-devtools'
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 import { AuthProvider } from '../components/authProvider'
 
 export const Route = createRootRoute({
   component: () => (
     <AuthProvider>
       <Outlet />
-      <TanStackDevtools
-        config={{
-          position: 'bottom-right',
-        }}
-        plugins={[
-          {
-            name: 'Tanstack Router',
-            render: <TanStackRouterDevtoolsPanel />,
-          },
-        ]}
-      />
+      {import.meta.env.DEV && <TanStackRouterDevtools />}
     </AuthProvider>
   ),
 })
